df80cbf6c3cdd1cec3807bc266304dea9532389e
Viviane Dias <vivianedias@pm.me>
2022-11-19 19:24:42 -0300

    feat: styles text and heading comp

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,80 @@
-const theme = {};
+import { defineStyleConfig } from "@chakra-ui/react";
+
+const Text = defineStyleConfig({
+  baseStyle: {
+    fontFamily: "body",
+  },
+  variants: {
+    body1: {
+      fontWeight: 400,
+      lineHeight: 0,
+    },
+    body2: {
+      fontWeight: 400,
+      lineHeight: "0.15px",
+    },
+    tooltip: {
+      fontWeight: 500,
+      lineHeight: 0,
+    },
+    subtitle1: {
+      fontWeight: 400,
+      lineHeight: "0.15px",
+    },
+    subtitle2: {
+      fontWeight: 500,
+      lineHeight: "0.15px",
+    },
+  },
+  sizes: {
+    xs: {
+      fontSize: "10px",
+    },
+    sm: {
+      fontSize: "sm",
+    },
+    md: {
+      fontSize: "md",
+    },
+  },
+  defaultProps: {
+    variant: "body1",
+    size: "md",
+  },
+});
+
+const Heading = defineStyleConfig({
+  baseStyle: {
+    fontFamily: "heading",
+    lineHeight: 0,
+  },
+  variants: {
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 500,
+    },
+    h3: {
+      fontWeight: 500,
+    },
+  },
+  sizes: {
+    md: {
+      fontSize: "18px",
+    },
+  },
+});
+
+const theme = {
+  fonts: {
+    heading: `"Crimson Pro", serif`,
+    body: `"Inter", sans-serif`,
+  },
+  components: {
+    Text,
+    Heading,
+  },
+};
 
 export default theme;
