e860ac470570d749b078d0d778dccd89de00cef5
Viviane Dias <vivianedias@pm.me>
2022-11-20 10:45:13 -0300

    feat(theme): adds new color pallete

--- src/theme.ts
+++ src/theme.ts
@@ -11,7 +11,7 @@ const Text = defineStyleConfig({
     },
     body2: {
       fontWeight: 400,
-      lineHeight: "0.15px",
+      lineHeight: "0.009rem",
     },
     tooltip: {
       fontWeight: 500,
@@ -19,16 +19,16 @@ const Text = defineStyleConfig({
     },
     subtitle1: {
       fontWeight: 400,
-      lineHeight: "0.15px",
+      lineHeight: "0.009rem",
     },
     subtitle2: {
       fontWeight: 500,
-      lineHeight: "0.15px",
+      lineHeight: "0.009rem",
     },
   },
   sizes: {
     xs: {
-      fontSize: "10px",
+      fontSize: "0.625rem",
     },
     sm: {
       fontSize: "sm",
@@ -74,6 +74,22 @@ const theme = {
   components: {
     Text,
     Heading,
+  },
+  colors: {
+    primary: {
+      300: "rgba(252, 218, 130, 1)", // gold
+      400: "rgba(221, 198, 146, 1)", // gold light
+      500: "rgba(198, 161, 91, 1)", // gold dark
+    },
+    secondary: {
+      50: "#F9F9F9", // gris
+      400: "rgba(162, 155, 142, 1)", // light beige
+      600: "rgba(59, 66, 74, 1)", // gris claro
+      700: "rgba(37, 37, 39, 1)", // dark gray
+      800: "rgba(4, 2, 16, 1)", // gris claro
+      900: "rgba(0, 0, 0, 1)", // black
+      red: "rgba(186, 49, 61, 1)",
+    },
   },
 };
 
