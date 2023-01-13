3c5665f81a581476af38a5f7b59041abc21a382f
Viviane Dias <vivianedias@pm.me>
2023-01-13 13:26:52 -0300

    feat(stamp): adds empty stamp

--- src/components/Stamps/index.tsx
+++ src/components/Stamps/index.tsx
@@ -6,3 +6,4 @@ export { default as RedMoney } from "./RedMoney";
 export { default as RedStamp } from "./RedStamp";
 export { default as GoldenLeaves } from "./GoldenLeaves";
 export { default as GoldenStamp } from "./GoldenStamp";
+export { default as EmptyStamp } from "./EmptyStamp";
