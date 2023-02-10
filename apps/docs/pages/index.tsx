652da7ea833eb53b1b29a4f8571f9b51e4f9a1a8
Viviane Dias <vivianedias@pm.me>
2023-02-10 16:44:00 -0300

    feat(docs): renders illustrations and icons in docs app

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,36 @@
-import { Button, Heading } from "ui";
+import { Button, Heading, Box, Illustrations, Icons } from "ui";
 
 export default function Docs() {
   return (
     <div>
       <Heading>Docs</Heading>
       <Button>boop</Button>
+
+      <Box bgColor={"white"}>
+        <Heading
+          as={"h3"}
+          variant={"h2"}
+          size={"md"}
+          textAlign={"center"}
+          pb={8}
+          color={"secondary.700"}
+        >
+          Icons
+        </Heading>
+        <Box>
+          <Illustrations.Hat boxSize={"4xs"} />
+          <Illustrations.Feather boxSize={"4xs"} />
+          <Illustrations.Potion boxSize={"4xs"} />
+          <Illustrations.Owl boxSize={"4xs"} />
+        </Box>
+        <Box>
+          {Object.keys(Icons).map((IconName, i) => {
+            const Icon = Icons[IconName as keyof typeof Icons];
+
+            return <Icon key={`${IconName}-${i}`} boxSize={8} />;
+          })}
+        </Box>
+      </Box>
     </div>
   );
 }
