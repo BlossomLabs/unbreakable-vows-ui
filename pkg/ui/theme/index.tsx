eaf99f8c972f7b57b9952391aaaddb3fec9e9ef0
Viviane Dias <vivianedias@pm.me>
2023-02-10 16:08:06 -0300

    chore(fonts): installs fonts used by theme

--- pkg/ui/theme/index.tsx
+++ pkg/ui/theme/index.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/crimson-pro/500.css";
+import "@fontsource/crimson-pro/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import theme from './customTheme'
 
