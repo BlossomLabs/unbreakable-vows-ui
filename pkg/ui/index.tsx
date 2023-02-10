a2d5265f8c970b7bc3fbfd866a5739b84d9edb40
Viviane Dias <vivianedias@pm.me>
2023-02-10 16:54:42 -0300

    feat(ui): creates components folder and moves icons and illustrations there

--- pkg/ui/index.tsx
+++ pkg/ui/index.tsx
@@ -1,6 +1,6 @@
 export * from '@chakra-ui/react'
 
-export * as Icons from "./Icons";
-export * as Illustrations from "./Illustrations";
+export * as Icons from "./components/Icons";
+export * as Illustrations from "./components/Illustrations";
 
 export { default as ThemeProvider } from './theme'
