75d56d099b87da1014455b3652b79d38edc29222
Viviane Dias <vivianedias@pm.me>
2023-02-14 15:10:34 -0300

    feat(contract): adds final contract ornaments svg

--- pkg/ui/components/Illustrations/Contract/index.ts
+++ pkg/ui/components/Illustrations/Contract/index.ts
@@ -1,3 +1,5 @@
 export { default as DecorationLeft } from "./DecorationLeft";
 export { default as DecorationRight } from "./DecorationRight";
 export { default as Ornament } from "./Ornament";
+export { default as OrnamentSmall } from "./OrnamentSmall";
+export { default as TitleDecoration } from "./TitleDecoration";
